4472e331159427e4b7318395bebec0ba52717700
bro-linux <genezisuz@gmail.com>
2023-01-01 14:05:27 -0500

    changed

--- server/server.js
+++ server/server.js
@@ -34,7 +34,7 @@ app.post("/", async (req, res) => {
             temperature:0,
             max_tokens:3000,
             top_p:1,
-            frequency_penalty:5,
+            frequency_penalty:0.5,
             presence_penalty:0,
         });
         res.status(200).send({
