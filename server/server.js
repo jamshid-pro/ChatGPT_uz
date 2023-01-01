783f486daf8bda544ea6b53a5ad5f0fbf6ead05f
bro-linux <genezisuz@gmail.com>
2023-01-01 14:02:57 -0500

    changed

--- server/server.js
+++ server/server.js
@@ -38,7 +38,7 @@ app.post("/", async (req, res) => {
             presence_penalty:0,
         });
         res.status(200).send({
-            bot: response.data.choices[0].text,
+            bot: response.data.choices[0].text
         });
     } catch (error) {
         console.log(error);
