80cc0594d4479dfc23dc1bff80b92c203e29367c
bro-linux <genezisuz@gmail.com>
2023-01-05 12:48:51 -0500

    commit 4 css changed

--- client/script.js
+++ client/script.js
@@ -76,7 +76,7 @@ const handleSubmit = async(e) => {
 
   loader(messageDiv);
 
-  const response = await fetch('https://chatgpt-uz.onrender.com/', {
+  const response = await fetch('http://localhost:5000/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
