5f2b6cf49214f172a184b0ced743ce594d657bf1
bro-linux <genezisuz@gmail.com>
2023-01-05 13:06:46 -0500

    added dotenv

--- client/script.js
+++ client/script.js
@@ -1,5 +1,8 @@
 import bot from "./assets/bot.svg";
 import user from "./assets/user.svg";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const form = document.querySelector('form');
 const chatContainer = document.querySelector("#chat_container");
