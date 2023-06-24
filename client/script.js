3b2113af0ada3eb19c11625242c616ad59548886
Jamshid <105177958+jamshid-pro@users.noreply.github.com>
2023-06-24 12:48:59 +0500

    Update script.js
    changes link

--- client/script.js
+++ client/script.js
@@ -77,7 +77,7 @@ const handleSubmit = async(e) => {
 
   loader(messageDiv);
 
-  const response = await fetch("https://chatgpt-uz.onrender.com", {
+  const response = await fetch("https://chatgpt-chapayev.onrender.com", {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
